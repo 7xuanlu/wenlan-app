a84a2693b338c700a6a442cf27766d1839538ae5
7xuanlu <h164654156465@gmail.com>
2026-06-28 18:14:26 -0700

    fix: format space delete command

--- app/src/search.rs
+++ app/src/search.rs
@@ -2391,10 +2391,7 @@ pub async fn update_space(
 }
 
 #[tauri::command]
-pub async fn delete_space(
-    state: tauri::State<'_, State>,
-    name: String,
-) -> Result<(), String> {
+pub async fn delete_space(state: tauri::State<'_, State>, name: String) -> Result<(), String> {
     let client = {
         let s = state.read().await;
         s.client.clone()
